3308d5215deac1236dcbf2562432e363dd02f2aa
Eric Ahn <erah@microsoft.com>
2019-07-09 16:39:09 -0700

    Emojify the weather reports and use api.weather.gov instead of hacky bs4 parsing

--- daily_post.py
+++ daily_post.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+from typing import Any, Dict, Optional
 import datetime
+import json
+import re
 
-from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader
 import praw
 import requests
@@ -10,29 +12,76 @@ from bot import *
 import daily_markov
 
 
-def get_weather() -> str:
-    resp = requests.get(
-        'http://forecast.weather.gov/MapClick.php?lat=47.62&lon=-122.36&unit=0&lg=english&FcstType=text&TextType=1')
-    doc = resp.content
-    soup_doc = BeautifulSoup(doc, 'html.parser')
-    weather_info = soup_doc.find_all('table')[1]
-    reddit_comment = ''
-    if weather_info.find_all('div'):
-        advisories = weather_info.div.extract().find_all('a')
-        reddit_comment += '* Advisories:\n'
-        for advis in advisories:
-            reddit_comment += ' * [{}](http://forecast.weather.gov/{})\n'.format(
-                advis.span.string, advis.get('href'))
-    forecast = str(weather_info).split("<br/>\n<br/>")
-    top_three = '<br/>\n<br/>'.join(str(x) for x in forecast[1:5])
-    top_three_doc = BeautifulSoup(top_three, 'html.parser')
-    reddit_comment += '* {}'.format(str.replace(
-        top_three_doc.text, '\n\n', "\n* ")[1:-1])
-
-    return reddit_comment
+FORECAST_URL = 'https://api.weather.gov/gridpoints/SEW/123,68/forecast'
 
 
-def gen_post(weekday: int) -> str:
+class Emojis:
+    NewMoon = '\U0001F311'
+    Sunny = '\u2600'
+    Cloudy = '\u2601'
+    PartlyCloudy = '\u26C5'
+    RainAndLightning = '\u26C8'
+    Rain = '\U0001F327'
+    Snow = '\U0001F328'
+    Lightning = '\U0001F329'
+    Fog = '\U0001F32B'
+
+
+def _convert_time_str_to_datetime(time: str) -> datetime.datetime:
+    time = re.sub(r'([-+]\d\d):(\d\d)', r'\1\2', time)
+    return datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%S%z')
+
+
+def get_weather_emoji(period: Dict[str, Any]) -> Optional[str]:
+    # only get the first phrase in the short forecast
+    match = re.fullmatch(r'((?:[A-Z]\w+ )+[A-Z]\w+)', period['shortForecast'])
+    if match is None:
+        return None
+
+    night = period['isDaytime']
+    short_forecast = match.group(1).lower()
+
+    rainy_regex = r'.*rain.*'
+    cloudy_regex = r'(?:mostly )?cloudy'
+    partly_cloudy_regex = r'partly sunny'
+
+    if re.fullmatch(rainy_regex, short_forecast):
+        return Emojis.Rain
+    elif re.fullmatch(cloudy_regex, short_forecast):
+        return Emojis.Cloudy
+    elif re.fullmatch(partly_cloudy_regex, short_forecast):
+        return Emojis.PartlyCloudy
+    else:
+        return None
+
+
+def get_weather(client: praw.Reddit) -> str:
+    response = requests.get(FORECAST_URL)
+    data = response.json()
+
+    # TODO: I vaguely remember there being a way to get advisories. Can't find it right now for some reason though
+    current_date = datetime.date.today()
+    forecast_periods = [period for period in data['properties']['periods']
+                        if _convert_time_str_to_datetime(period['startTime']).date() >= current_date][:4]
+    comment_lines = []
+    emoji_fails = []
+
+    for period in forecast_periods:
+        name, forecast = period['name'], period['detailedForecast']
+        emoji = get_weather_emoji(period)
+        if emoji is not None:
+            comment_lines.append('* {}: {} {}'.format(name, emoji, forecast))
+        else:
+            emoji_fails.append(period)
+            comment_lines.append('* {}: {}'.format(name, forecast))
+
+    # We might not have been able to emojify the weather, send a PM so that we can fix it
+    if emoji_fails:
+        client.redditor('wchill').message('Emojification failed!', json.dumps(emoji_fails))
+    return '\n'.join(comment_lines)
+
+
+def gen_post(weekday: int, client: praw.Reddit) -> str:
     j2_env = Environment(loader=FileSystemLoader('./templates'), trim_blocks=True)
     template = j2_env.get_template('daily_post.j2')
 
@@ -41,7 +90,9 @@ def gen_post(weekday: int) -> str:
     else:
         quote_of_the_day = daily_markov.get_quote(subreddit)
 
-    return template.render(day_of_week=weekday, forecast=get_weather(), qotd=quote_of_the_day)
+    forecast = get_weather(client)
+
+    return template.render(day_of_week=weekday, forecast=forecast, qotd=quote_of_the_day)
 
 
 if __name__ == '__main__':
@@ -54,7 +105,7 @@ if __name__ == '__main__':
     subreddit = reddit.subreddit(REDDIT_SUB)
 
     now = datetime.datetime.now()
-    post = gen_post(now.weekday())
+    post = gen_post(now.weekday(), reddit)
 
     subreddit.submit(title=now.strftime("Seattle Reddit Community Open Chat, %A, %B %d, %Y"),
                      selftext=post,
